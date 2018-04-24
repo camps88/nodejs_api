2ad791f318f13938190bd5216ea7d919815727c9
JuanjoZanabria <jjzanabria@alumnos.upm.es>
2018-04-24 21:47:14 +0200

    Primera version del server.js y del googleClient.js

---
+++ googleClient.js
@@ -0,0 +1,43 @@
+const vision = require('@google-cloud/vision');
+const credentials = {
+  keyFilename: '../gaptfgkey.json'
+};
+
+const client = new vision.ImageAnnotatorClient(credentials);
+class googleClient {
+
+  constructor() {}
+
+  setRequestFeature(featureType) {
+    const feature = formatFeature(featureType)
+    const request = {
+      image: {
+        source: {
+          imageUri: 'gs://gapbbdd/IMG_6577_ok.jpg'
+        }
+      },
+      features: [{
+        feature
+      }],
+    }
+  }
+  getImageAnnotated(request) {
+    client
+      .annotateImage(request)
+      .then(results => {
+        console.log(JSON.stringify(results));
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
+  }
+  formatFeature(formatFeature) {
+    return JSON.stringify(formatFeature)
+  }
+
+  print(str){
+    console.log(str);
+  }
+}
+
+module.exports = googleClient;
