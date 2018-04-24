e7b6aa57de39c7efc68245d31afca518e7bb2ea0
JuanjoZanabria <jjzanabria@alumnos.upm.es>
2018-04-24 23:10:57 +0200

    Commit 1 24-Abr Juanjo Refactorizar código

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const googleClient = require('./googleClient');
 const server = express();
 const instanceGoogleClient = new googleClient();
 
-server.get("/", (req, res) => {
+server.get("/", function (req, res) {
   console.log("Responding to root route");
-  instanceGoogleClient.print("pepe");
-  res.send("Hello from Rooooot!");
+  instanceGoogleClient.print("Task 0: Create GoogleClient --- Successful");
+  res.send("Hello from Root!");
 })
 
-server.listen(3003, () => {
+server.listen(3003, function () {
   console.log("Server is up and listening on 3003...");
 })
